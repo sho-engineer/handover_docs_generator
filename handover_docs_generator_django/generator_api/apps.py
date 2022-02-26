21bea72805ae7cda2f3155329ad4eb9740c79c7a
sho-engineer <arlonbolt@gmail.com>
2022-02-26 21:11:17 +0900

    [complete] settings before start developping

---
+++ handover_docs_generator_django/generator_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GeneratorApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'generator_api'
--- handover_docs_generator_django/handover_docs_generator_django/settings.py
+++ handover_docs_generator_django/handover_docs_generator_django/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'generator_api'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (  
+    'http://localhost:3000', 
+)
+
 
 ROOT_URLCONF = 'handover_docs_generator_django.urls'
 
@@ -123,3 +132,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = { 
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
