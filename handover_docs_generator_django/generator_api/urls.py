d9fbbef3204da7f997411a712d354e0f94d6913c
sho-engineer <arlonbolt@gmail.com>
2022-03-02 00:46:53 +0900

    [add]Login/Logout processing

--- handover_docs_generator_django/generator_api/urls.py
+++ handover_docs_generator_django/generator_api/urls.py
@@ -1,5 +1,8 @@
+from unicodedata import name
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('login/', views.Longin.as_view(), name="login"),
+    path('logout/', views.Logout.as_view(), name="logout")
 ]
--- handover_docs_generator_django/generator_api/views.py
+++ handover_docs_generator_django/generator_api/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import(LoginView,LogoutView)
-from forms import LoginForm
+from . import forms
 
 # Create your views here.
 class Longin(LoginView):
-    form_class = LoginForm
+    form_class = forms.LoginForm
+    template_name = "accounts/login.html"
 
 class Logout(LoginRequiredMixin,LogoutView):
     pass
