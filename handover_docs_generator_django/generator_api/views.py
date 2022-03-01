dd35572d265b5c53c9562ed947c921820bbc96ff
sho-engineer <arlonbolt@gmail.com>
2022-03-01 23:55:20 +0900

    [add] Login Form/ LoginView

--- handover_docs_generator_django/generator_api/views.py
+++ handover_docs_generator_django/generator_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import(LoginView,LogoutView)
+from forms import LoginForm
+
 # Create your views here.
+class Longin(LoginView):
+    form_class = LoginForm
+
+class Logout(LoginRequiredMixin,LogoutView):
+    pass
