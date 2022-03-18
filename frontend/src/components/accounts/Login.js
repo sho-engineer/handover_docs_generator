647a0cace6a330eb5daf4ad4db11cd6ed7e3f704
sho-engineer <arlonbolt@gmail.com>
2022-03-18 23:30:41 +0900

    [add]react-router-dom/material ui/ material icon

---
+++ frontend/src/components/accounts/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Grid } from '@mui/material'
+
+const Login = () => {
+  return (
+    <Grid>
+        <Paper elevation={10}>
+
+        </Paper>
+    </Grid>
+  )
+}
+
+export default Login
