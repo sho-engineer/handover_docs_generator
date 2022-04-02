544d94cd6aef1850c76fcdcbfba6406b58c7d21c
sho-engineer <arlonbolt@gmail.com>
2022-04-03 00:03:40 +0900

    [add]AppBarにタブを追加

--- frontend/src/components/accounts/Login.js
+++ frontend/src/components/accounts/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 const Login = () => {
-  const [test, setTest] = useState("test");
+
   return (
    <div>
      <h1>テスト</h1>
--- frontend/src/components/layouts/MenuAppBar.js
+++ frontend/src/components/layouts/MenuAppBar.js
@@ -1,35 +1,59 @@
-import React from 'react'
-import { AppBar } from '@mui/material'
-import { Box } from '@mui/system'
-import IconButton from '@mui/material/IconButton';
+import React from 'react';
+import { AppBar } from '@mui/material';
+import { Box } from '@mui/system';
+import { useState } from 'react';
 import styled from 'styled-components';
-import MenuIcon from '@mui/icons-material/Menu'
+import { Link, Route } from 'react-router-dom';
+import { Tab, Tabs } from '@mui/material';
+import Home from '../../Home';
+import About from '../../About';
 
 const MenuAppBar = () => {
 
   const HeaderTitle = styled.h2`
     font-weight: bold;
-    margin: 20px 30px;
+    margin: 5px 35px;
+    font-family: 'Cherry Swash', cursive;
+    width:280px;
   `
-  const AppBarStyled = styled(AppBar)`
+  const StyledAppBar = styled(AppBar)`
     background-color: #696969;
-  `;
+  `
+  const StyledFont = styled.span`
+    font-size: 40px;
+  ` 
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  console.log(value);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBarStyled position="static">
-
-          <IconButton
-                size="medium"
-                arialabel="menu"
-                color="default"
-                position="fixed"
-                sx={{ mr: 2 }}
-            >
-            </IconButton>
-        <HeaderTitle>引き継ぎDocs Generator</HeaderTitle>
-      </AppBarStyled> 
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <StyledAppBar position="static">
+        <HeaderTitle><StyledFont>T</StyledFont>akeover <StyledFont>D</StyledFont>ocs <StyledFont>G</StyledFont>enerator</HeaderTitle>
+        <Tabs value={value} 
+          textColor="default"
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: '#FFFFFF',
+              height: '2px'
+            }
+          }} 
+          onChange={handleChange} 
+          centered
+          >
+          <Tab label="HOME" />
+          <Tab label="ABOUT" />
+        </Tabs>
+        </StyledAppBar>
+      </Box>
+      { value === 0 && <Home /> }
+      { value === 1 && <About /> }
+    </>
   )
 }
 
